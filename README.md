1b3c3c1ca52e0145b25bac83ad5ef0aba2cc2349
ix@nawi.at <ix@nawi.at>
2014-08-03 21:41:28 +0200

    hooks: added xautounlock to startup (depends on xautounlock and slock); README.md: started to deocument the functions

--- README.md
+++ README.md
@@ -2,3 +2,43 @@ qtileConfig
 ===========
 
 My modular qtile (0.7) config.py
+
+qtile_config.groups
+-------------------
+8 virtual desktops 'a s d f u i o p'
+
+qtile_config.hooks
+------------------
+* isRunning(process) - checks if process is running
+* executeOnce(process, *args) - executes process if it is not running
+* startup() - executes on startup (needs rewriting to startup(process))
+* mousePointer() - display a left pointer as mouse
+* floating_dialogs(window) - make 'dialog' clients float
+
+qtile_config.keys
+-----------------
+* mod+{asdfuiop} - change to the corresponding virtual desktop
+* mod,shift+{asdfuiop} - send active client to the corresponding virtual desktop
+* mod+{vbnm} - grow, shrink, normalize, maximize the client
+* mod,shift+M - flip the panes
+* mod+Tab - next layout
+* mod,shift+F - toggle floating
+* mod+w - kill the active client
+* mod+Return - spawn the app.screen()
+* mod+c - spawn the app.browser
+* mod+x - spawn the dmenu-style cmdline
+* mod+z - spawn the app.terminal
+
+qtile_config.layouts
+--------------------
+
+qtile_config.modifiers
+----------------------
+mod = mod4
+
+qtile_config.mouse
+------------------
+
+qtile_config.screens
+--------------------
+
--- qtile_config/hooks.py
+++ qtile_config/hooks.py
@@ -1,35 +1,49 @@
 from libqtile import hook
-import subprocess, re
+import subprocess
+import re
+
 
 def isRunning(process):
     '''checks if process is running'''
-    s = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
-    for proc in s.stdout:
+    subprocs = subprocess.Popen(["ps", "axw"], stdout=subprocess.PIPE)
+
+    for proc in subprocs.stdout:
         if re.search(process, proc):
             return True
     return False
 
+
 def executeOnce(process, *args):
-    '''executes process if it is not running'''
+    ''' executes process if it is not running '''
     if not isRunning(process):
         options = " ".join(args)
         process = " ".join((process, options))
         return subprocess.Popen(process.split())
 
-# Execute on Startup
+
+#  Execute on Startup
 @hook.subscribe.startup
 def startup():
-    '''executes on startup'''
+    ''' executes on startup '''
     executeOnce("wpa_gui", "-t")
+
 
 @hook.subscribe.startup
 def mousePointer():
-    '''display a left pointer as mouse'''
+    ''' display a left pointer as mouse '''
     subprocess.Popen(['xsetroot', '-cursor_name', 'left_ptr'])
 
-# Execute when new client is spawned
+
+@hook.subscribe.startup
+def lockOnIdle():
+    ''' calls xautolock to slock after a given time (in min) of inactivity'''
+    executeOnce("xautolock", "-time 5", "-locker slock")
+
+
+#  Execute when new client is spawned
 @hook.subscribe.client_new
 def floating_dialogs(window):
+    ''' sets dialog clients to 'floating' '''
     dialog = window.window.get_wm_type() == 'dialog'
     transient = window.window.get_wm_transient_for()
     if dialog or transient:
